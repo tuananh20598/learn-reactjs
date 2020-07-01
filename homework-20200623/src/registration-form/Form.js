5dadfdffef4e425a422f9cdf72e2bf78e5e84053
TuanAnh205 <tuananhphan20598@gmail.com>
2020-07-01 20:54:49 +0700

    Optimize Code

--- homework-20200623/src/registration-form/Form.js
+++ homework-20200623/src/registration-form/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import "./style.css";
 
 class Form extends Component {
@@ -9,65 +9,29 @@ class Form extends Component {
       name: "",
       email: "",
       pass: "",
-      avatar: "",
+      avatar: [],
       phone: "",
       age: 1,
       gender: "male",
-      favorite: {
-        soccer: false,
-        badminton: false,
-        sailing: false,
-      },
+      favorites: [
+        { id: 1, value: "soccer", isChecked: false, name: "Đá bóng" },
+        { id: 2, value: "badminton", isChecked: false, name: "Cầu lông" },
+        { id: 3, value: "sailing", isChecked: false, name: "Đua thuyền" },
+      ],
       note: "Buổi học đầu tiên về html",
     };
   }
 
-  handleOnChangeName = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handleOnChangeEmail = (event) => {
-    this.setState({
-      email: event.target.value,
-    });
-  };
-
-  handleOnChangePass = (event) => {
-    this.setState({
-      pass: event.target.value,
-    });
-  };
-
-  handleOnChangeAvatar = (event) => {
-    this.setState({
-      avatar: event.target.value,
-    });
-  };
-
-  handleOnChangePhone = (event) => {
-    this.setState({
-      phone: event.target.value,
-    });
-  };
-
-  handleOnChangeAge = (event) => {
-    this.setState({
-      age: event.target.value,
-    });
-  };
-
-  handleOnChangeGender = (event) => {
-    this.setState({
-      gender: event.target.value,
-    });
-  };
-
-  handleOnChangeNote = (event) => {
-    this.setState({
-      note: event.target.value,
-    });
+  handleOnChangeInput = (event) => {
+    if (event.target.type === "file") {
+      this.setState({
+        avatar: event.target.files[0],
+      });
+    } else {
+      this.setState({
+        [event.target.name]: event.target.value,
+      });
+    }
   };
 
   handleOnChangeSoccer = (event) => {
@@ -122,158 +86,172 @@ class Form extends Component {
       note,
     } = this.state;
     return (
-      <div className="register">
-        <h1>Đăng kí thành viên</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Nội dung</th>
-              <th>Giá trị</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <label>Họ tên:</label>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  value={name}
-                  onChange={this.handleOnChangeName}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Email:</label>
-              </td>
-              <td>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={this.handleOnChangeEmail}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Mật khẩu:</label>
-              </td>
-              <td>
-                <input
-                  type="password"
-                  value={pass}
-                  onChange={this.handleOnChangePass}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Avatar:</label>
-              </td>
-              <td>
-                <input
-                  type="file"
-                  value={avatar}
-                  onChange={this.handleOnChangeAvatar}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Số điện thoại:</label>
-              </td>
-              <td>
-                <input
-                  type="number"
-                  value={phone}
-                  onChange={this.handleOnChangePhone}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Tuổi:</label>
-              </td>
-              <td>
-                <select value={age} onChange={this.handleOnChangeAge}>
-                  <option value="1">1 tuổi</option>
-                  <option value="2">2 tuổi</option>
-                  <option value="3">3 tuổi</option>
-                  <option value="4">4 tuổi</option>
-                </select>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Giới tính:</label>
-              </td>
-              <td>
-                <input
-                  type="radio"
-                  value="male"
-                  checked={gender === "male"}
-                  onChange={this.handleOnChangeGender}
-                />
-                Nam
-                <br />
-                <input
-                  type="radio"
-                  value="female"
-                  checked={gender === "female"}
-                  onChange={this.handleOnChangeGender}
-                />{" "}
-                Nữ
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Sở thích:</label>
-              </td>
-              <td>
-                <input
-                  type="checkbox"
-                  value="soccer"
-                  defaultChecked={favorite.soccer}
-                  onClick={this.handleOnChangeSoccer}
-                />{" "}
-                Đá bóng <br />
-                <input
-                  type="checkbox"
-                  value="badminton"
-                  defaultChecked={favorite.badminton}
-                  onClick={this.handleOnChangeBadminton}
-                />{" "}
-                Cầu lông <br />
-                <input
-                  type="checkbox"
-                  value="sailing"
-                  defaultChecked={favorite.sailing}
-                  onClick={this.handleOnChangeSailing}
-                />{" "}
-                Đua thuyền
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label>Ghi chú:</label>
-              </td>
-              <td>
-                <textarea
-                  rows="3"
-                  cols="25"
-                  value={note}
-                  onChange={this.handleOnChangeNote}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <button type="submit" onClick={this.handleOnSubmit}>
-          Đăng Ký
-        </button>
-      </div>
+      <form onSubmit={this.handleOnSubmit}>
+        <div className="register">
+          <h1>Đăng kí thành viên</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Nội dung</th>
+                <th>Giá trị</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <label>Họ tên:</label>
+                </td>
+                <td>
+                  <input
+                    name="name"
+                    type="text"
+                    value={name}
+                    onChange={this.handleOnChangeInput}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Email:</label>
+                </td>
+                <td>
+                  <input
+                    name="email"
+                    type="email"
+                    value={email}
+                    onChange={this.handleOnChangeInput}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Mật khẩu:</label>
+                </td>
+                <td>
+                  <input
+                    name="pass"
+                    type="password"
+                    value={pass}
+                    onChange={this.handleOnChangeInput}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Avatar:</label>
+                </td>
+                <td>
+                  <input
+                    name="file"
+                    type="file"
+                    value={avatar}
+                    onChange={this.handleOnChangeInput}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Số điện thoại:</label>
+                </td>
+                <td>
+                  <input
+                    name="phone"
+                    type="number"
+                    value={phone}
+                    onChange={this.handleOnChangeInput}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Tuổi:</label>
+                </td>
+                <td>
+                  <select
+                    name="age"
+                    value={age}
+                    onChange={this.handleOnChangeInput}
+                  >
+                    <option value="1">1 tuổi</option>
+                    <option value="2">2 tuổi</option>
+                    <option value="3">3 tuổi</option>
+                    <option value="4">4 tuổi</option>
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Giới tính:</label>
+                </td>
+                <td>
+                  <input
+                    name="gender"
+                    type="radio"
+                    value="male"
+                    checked={gender === "male"}
+                    onChange={this.handleOnChangeInput}
+                  />
+                  Nam
+                  <br />
+                  <input
+                    name="gender"
+                    type="radio"
+                    value="female"
+                    checked={gender === "female"}
+                    onChange={this.handleOnChangeInput}
+                  />
+                  Nữ
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Sở thích:</label>
+                </td>
+                <td>
+                  <input
+                    type="checkbox"
+                    value="soccer"
+                    defaultChecked={favorite.soccer}
+                    onClick={this.handleOnChangeSoccer}
+                  />
+                  Đá bóng <br />
+                  <input
+                    type="checkbox"
+                    value="badminton"
+                    defaultChecked={favorite.badminton}
+                    onClick={this.handleOnChangeBadminton}
+                  />
+                  Cầu lông <br />
+                  <input
+                    type="checkbox"
+                    value="sailing"
+                    defaultChecked={favorite.sailing}
+                    onClick={this.handleOnChangeSailing}
+                  />
+                  Đua thuyền
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Ghi chú:</label>
+                </td>
+                <td>
+                  <textarea
+                    name="note"
+                    rows="3"
+                    cols="25"
+                    value={note}
+                    onChange={this.handleOnChangeInput}
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <button type="submit" onClick={this.handleOnSubmit}>
+            Đăng Ký
+          </button>
+        </div>
+      </form>
     );
   }
 }
